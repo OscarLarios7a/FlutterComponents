90e7053d2e1f2cbb10900365e433c26a746d5625
Oscar Larios <javier.atoaxaca@gmail.com>
2021-10-23 15:13:32 -0500

    Se agrego nuvos widgets al archivo alert_page.dart

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -12,9 +12,18 @@ class AlertPage extends StatelessWidget {
           //   color: Colors.red.shade800,
           // ),
           centerTitle: true,
-          title: const Text(
-            'Alert Page',
-          )),
+          title: const Text('Alert Page')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Mostrar alerta'),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            onPrimary: Colors.white,
+            shape: StadiumBorder(),
+          ),
+          onPressed: () => _mostrarAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_location),
         onPressed: () {
@@ -22,5 +31,47 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  //*Se genera un metodo para mostras en el onPress
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+       
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Este es el contenido de la Caja de alerta'),
+                FlutterLogo(size: 100.0),
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                child: const Text('Cancelar'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                child: const Text('Ok'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
   }
 }
